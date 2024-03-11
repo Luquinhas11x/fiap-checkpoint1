d8aa0fd017884aaeb1d3e595845ae1dd58b4e8f8
Lucas <lukinha11x@gmail.com>
2024-03-11 15:58:33 -0300

    Adding diferent return endpoint response based on profile

--- src/main/java/br/com/fiap/checkpoint1/controller/Checkpoint1Controller.java
+++ src/main/java/br/com/fiap/checkpoint1/controller/Checkpoint1Controller.java
@@ -1,5 +1,6 @@
 package br.com.fiap.checkpoint1.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +8,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RestController
 @RequestMapping
 public class Checkpoint1Controller {
+    @Value("${PROFILE}")
+    private String activeProfile;
 
     @GetMapping("/ping")
     public String ping() {
-        return "Pong";
+        return "Pong em " + activeProfile;
     }
 }
